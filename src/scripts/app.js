5ce1b476afcc677e1b394c9abeb59dee17ba7725
imonaar <kevinmonari22@gmail.com>
2022-02-02 16:13:26 +0300

    initial

---
+++ src/scripts/app.js
@@ -0,0 +1,72 @@
+const calculator = document.querySelector(".calculator");
+const keys = calculator.querySelector(".calculator__keys");
+const display = document.querySelector(".calculator__display");
+
+keys.addEventListener("click", (e) => {
+  if (e.target.matches("button")) {
+    const key = e.target;
+    const action = key.dataset.action;
+    const keyContent = key.textContent;
+    const displayedNum = display.textContent;
+
+    const previousKeyType = calculator.dataset.previousKeyType;
+
+    Array.from(key.parentNode.children).forEach((k) =>
+      k.classList.remove("is-depressed")
+    );
+
+    if (!action) {
+      if (displayedNum === "0" || previousKeyType === "operator") {
+        display.textContent = keyContent;   
+      } else {
+        display.textContent = displayedNum + keyContent;
+      }
+    }
+
+    if (
+      action === "add" ||
+      action === "subtract" ||
+      action === "multiply" ||
+      action === "divide"
+    ) {
+      key.classList.add("is-depressed");
+      calculator.dataset.previousKeyType = "operator";
+      calculator.dataset.firstValue = displayedNum;
+      calculator.dataset.operator = action;
+    }
+
+    if (action === "decimal") {
+      display.textContent = displayedNum + ".";
+    }
+
+    if (action === "clear") {
+      console.log("clear key!");
+    }
+
+    if (action === "calculate") {
+      const firstValue = parseFloat(calculator.dataset.firstValue);
+      const operator = calculator.dataset.operator;
+      const secondValue = parseFloat(displayedNum);
+
+      display.textContent = calculate(firstValue, operator, secondValue);
+    }
+  }
+});
+
+function calculate(n1, operator, n2) {
+  let result;
+  switch (operator) {
+    case "add":
+      result = n1 + n2;
+    case "subtract":
+      result = n1 - n2;
+    case "multiply":
+      result = n1 * n2;
+    case "divide":
+      result = n1 / n2;
+    default:
+      return result;
+  }
+
+  return result;
+}
